7401f6df95d1155bb953708eac85a9a8ddfd75fc
soohoin <soohoin12@naver.com>
2021-06-26 07:29:40 +0900

    Firebase Admin SDK 설정

---
+++ src/main/java/com/example/testsnslogin_server/config/FirebaseConfig.java
@@ -0,0 +1,32 @@
+package com.example.testsnslogin_server.config;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void init() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+            FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+            FirebaseApp.initializeApp(options);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+}
