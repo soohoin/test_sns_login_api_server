cc900234fac802bad9fc7b13dbb2eb50469bff67
soohoin <soohoin12@naver.com>
2021-06-25 12:30:00 +0900

    spring boot api server setting

---
+++ src/test/java/com/example/testsnslogin_server/TestsnsloginServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsnslogin_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsnsloginServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
