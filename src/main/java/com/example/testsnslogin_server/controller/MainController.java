354d4193403a5b242e95f7a8af900847defe26f6
soohoin <soohoin12@naver.com>
2021-06-29 13:25:13 +0900

    MainController 변경

--- src/main/java/com/example/testsnslogin_server/controller/MainController.java
+++ src/main/java/com/example/testsnslogin_server/controller/MainController.java
@@ -6,13 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 public class MainController {
 
     @Autowired
@@ -20,23 +21,19 @@ public class MainController {
     
 
     @RequestMapping("test")
+    @ResponseBody
     public String testConnect() {
 
         return "연결성공";
     }
 
     @RequestMapping("kakao/sign_in")
-    public ModelAndView kakaoSignIn(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse response, RedirectAttributes attributes) {
+    public String kakaoSignIn(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse response, RedirectAttributes attributes) {
         System.out.println("=========================");
 
-        kakaoService.execKakaoLogin(code);
+        // kakaoService.execKakaoLogin(code);
 
 
-        return null;
+        return "redirect:webauthcallback://success?12345";
     }
-
-    public static JsonNode getAccessToken(String autorize_code) { 
-        return null; 
-    }
-
 }
