d251ee8dbb02549cf6a11d65ad043c935d7fe938
soohoin <soohoin12@naver.com>
2021-06-30 22:34:24 +0900

    MainController.java 수정

--- src/main/java/com/example/testsnslogin_server/controller/MainController.java
+++ src/main/java/com/example/testsnslogin_server/controller/MainController.java
@@ -31,7 +31,6 @@ public class MainController {
 
         Map<String,Object> result =  kakaoService.execKakaoLogin(code);
 
-        kakaoService.logout(result.get("accessToken").toString());
         return "redirect:webauthcallback://success?accessToken"+result.get("accessToken").toString();
     }
 }
